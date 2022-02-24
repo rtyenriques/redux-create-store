38feffc60119fe3285f6a3f8145d4cdfb38923bc
= <rtyenriques@gmail.com>
2022-02-23 22:59:55 -0800

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,21 @@
+function createStore() {
 let state;
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+};
+
+function getState() {
+  return state;
+}
+
+return {
+  dispatch,
+  getState
+};
+
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +27,28 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+// function dispatch(action){
+//   state = reducer(state, action);
+//   render();
+// };
 
+// function render() {
+//   let container = document.getElementById('container');
+//   container.textContent = state.count;
+// };
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore();
+store.dispatch({ type: '@@INIT' });
+// dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+// button.addEventListener('click', function() {
+//     dispatch({ type: 'INCREASE_COUNT' });
+// })
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
 })
